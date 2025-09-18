0e6cbe7836ac6980bc546a140f2b51a87a19eca0
farhan03-s <farhanshaikshariff@gmail.com>
2025-09-18 11:04:24 +0530

    Create user-pass.py

---
+++ user-pass.py
@@ -0,0 +1,8 @@
+user=Farhan
+password=3306
+username=input("Enter your username: ")
+password=input("Enter your password: ")
+if user==username and password==password:
+    print("Login Successful")
+else:
+    print("Login Failed")
